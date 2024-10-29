5abe5098305f48fe11fe82f74fe04b695f3b983e
SEO DAE WON <cryingfor@gmail.com>
2024-10-29 13:44:07 +0900

    Update Search.tsx
    onClick 에 전달하는 인자의 형태가 함수를 감싸서 전달.

--- Day01/src/components/movies/Search.tsx
+++ Day01/src/components/movies/Search.tsx
@@ -4,11 +4,6 @@ import { useMovieStore } from '@/stores/movie'
 export default function Search() {
   const [searchText, setSearchText] = useState('')
   const fetchMovies = useMovieStore(state => state.fectchMovies)
-  async function searchMovies() {
-    const data = await fetchMovies()
-    setMovies(data)
-    // console.log(data)
-  }
 
   return (
     <div>
@@ -17,7 +12,7 @@ export default function Search() {
         onChange={e => setSearchText(e.target.value)}
         onKeyDown={e => e.key === 'Enter' && fetchMovies(searchText)}
       />
-      <button onClick={fetchMovies(searchText)}>검색</button>
+      <button onClick={() => fetchMovies(searchText)}>검색</button>
     </div>
   )
 }
