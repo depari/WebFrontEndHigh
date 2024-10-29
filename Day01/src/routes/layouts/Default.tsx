2f8c02c27a943cea582b7cc84662871bbb6bc37b
SEO DAE WON <cryingfor@gmail.com>
2024-10-29 10:34:33 +0900

    Update Default.tsx
    oulet compoment -> oulet hook

--- Day01/src/routes/layouts/Default.tsx
+++ Day01/src/routes/layouts/Default.tsx
@@ -1,10 +1,13 @@
 import Header from '@/components/Header'
-import { Outlet } from 'react-router-dom'
+import { useOutlet } from 'react-router-dom'
+import { motion, AnimatePresence } from 'framer-motion'
+
 export default function Default() {
+  const outlet = useOutlet()
   return (
     <>
       <Header />
-      <Outlet />
+      {outlet}
     </>
   )
 }
