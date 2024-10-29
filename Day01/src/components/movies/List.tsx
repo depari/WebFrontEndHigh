275fb721328179d71ab4c39ca00aea629c72e0ee
SEO DAE WON <cryingfor@gmail.com>
2024-10-29 14:50:36 +0900

    loading 에니베이션 적용하기

--- Day01/src/components/movies/List.tsx
+++ Day01/src/components/movies/List.tsx
@@ -1,17 +1,25 @@
 import { Link } from 'react-router-dom'
 import { useMovieStore } from '@/stores/movie'
+import Loader from '../Loader'
 
 export default function List() {
   const movies = useMovieStore(state => state.movies)
+  const loading = useMovieStore(state => state.loading)
   return (
-    <ul>
-      {movies?.map(movie => {
-        return (
-          <li key={movie.imdbID}>
-            <Link to={`/movies/${movie.imdbID}`}>{movie.Title}</Link>
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <ul>
+          {movies?.map(movie => {
+            return (
+              <li key={movie.imdbID}>
+                <Link to={`/movies/${movie.imdbID}`}>{movie.Title}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      )}
+    </>
   )
 }
--- Day01/src/stores/movie.ts
+++ Day01/src/stores/movie.ts
@@ -12,17 +12,22 @@ export interface Movie {
 export const useMovieStore = create(
   combine(
     {
-      movies: [] as Movie[]
+      movies: [] as Movie[],
+      loading: false
     },
     set => {
       return {
         fetchMovies: async (searchText: string) => {
+          set({
+            loading: true
+          })
           const res = await fetch(
             `https://omdbapi.com/?apikey=7035c60c&s=${searchText}`
           )
           const { Search } = await res.json()
           set({
-            movies: Search
+            movies: Search,
+            loading: false
           })
         }
       }
