637f5f2573d18d2ca303c068e590bac561b29f58
SEO DAE WON <cryingfor@gmail.com>
2024-10-30 15:27:21 +0900

    Cached 처리

--- Project/src/App.tsx
+++ Project/src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
 import MovieList from './components/MovieList'
 
 export interface Movie {
@@ -11,43 +9,6 @@ export interface Movie {
 }
 
 export default function App() {
-  const [searchText, setSearchText] = useState('')
-
-  const { data, refetch } = useQuery<Movie[]>({
-    queryKey: ['movies'],
-    queryFn: async () => {
-      const res = await fetch(
-        `https://omdbapi.com/?apikey=7035c60c&s=${searchText}`
-      )
-      const { Search } = await res.json()
-      console.log(Search)
-      return Search
-    },
-    enabled: false,
-    staleTime: 1000 * 60, //1분
-    initialData: [
-      {
-        Title:
-          'Search 해보세요, Enter 치면 들어갑니다. 두글자 이상 입력 후 Enter 키!',
-        Year: '1984',
-        imdbID: 'tt0090190',
-        Type: 'movie',
-        Poster:
-          'https://m.media-amazon.com/images/M/MV5BNDZjMGZjMDktOTZlNC00MTQ5LTkzY2UtZjM1MjE0NDcwNzk1XkEyXkFqcGc@._V1_SX300.jpg'
-      }
-    ],
-    placeholderData: [
-      {
-        Title: '대기 중입니다. ',
-        Year: '1984',
-        imdbID: 'tt0090190',
-        Type: 'movie',
-        Poster:
-          'https://m.media-amazon.com/images/M/MV5BNDZjMGZjMDktOTZlNC00MTQ5LTkzY2UtZjM1MjE0NDcwNzk1XkEyXkFqcGc@._V1_SX300.jpg'
-      }
-    ]
-  })
-
   return (
     <>
       <MovieList id="01" />
--- Project/src/components/MovieList.tsx
+++ Project/src/components/MovieList.tsx
@@ -1,10 +1,19 @@
 import { useFetchMovies } from '@/hooks/movie'
+import { useQueryClient } from '@tanstack/react-query'
 
 export default function MovieList({ id }: { id: string | number }) {
   const {
     query: { data, refetch, isLoading, isFetching },
     searchText
   } = useFetchMovies(id)
+
+  const queryClient = useQueryClient()
+
+  function getCachedDta() {
+    const cachedData = queryClient.getQueryData(['movies', id])
+    console.log('getCachedData', cachedData)
+    // queryClient.getQueryData(query.queryKey)
+  }
   return (
     <>
       <h1>Movie List!!</h1>
@@ -16,6 +25,8 @@ export default function MovieList({ id }: { id: string | number }) {
       {data?.map((movie: Movie) => {
         return <div key={movie.imdbID}>{movie.Title}</div>
       })}
+
+      <button onClick={getCachedDta}>캐시된 데이터 Get</button>
     </>
   )
 }
