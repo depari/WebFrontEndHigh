cd72702674c9fd8775d76ed3418a949247a5e914
SEO DAE WON <cryingfor@gmail.com>
2024-10-31 15:52:49 +0900

    MovieInfinitList 로 분리

--- Project/src/App.tsx
+++ Project/src/App.tsx
@@ -1,54 +1,9 @@
-import { useRef, useEffect } from 'react'
-import { useFetchMovies } from '@/hooks/movie.infinite'
-import Loader from '@/components/Loader'
-import { useInView } from 'react-intersection-observer'
+import MovieInfinitList from '@/components/movies/MovieInfiinitList'
 
 export default function App() {
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isLoading,
-    searchText,
-    setSearchText,
-    refetch
-  } = useFetchMovies()
-
-  const { ref, inView } = useInView()
-
-  useEffect(() => {
-    if (isLoading) return
-    if (inView) {
-      fetchNextPage()
-    }
-  }, [inView])
-
-  function searchMovies() {
-    refetch()
-  }
   return (
     <>
-      <div>
-        <input
-          value={searchText}
-          onChange={e => setSearchText(e.target.value)}
-          onKeyDown={e => e.key === 'Enter' && searchMovies()}
-        />
-        <button onClick={() => searchMovies()}>검색</button>
-        {data?.pages.map(page => {
-          return page.Search.map(movie => {
-            return <div key={movie.imdbID}>{movie.Title}</div>
-          })
-        })}
-        {isLoading && <Loader />}
-        {!isLoading && hasNextPage && (
-          <button
-            ref={ref}
-            onClick={() => fetchNextPage()}>
-            더보기
-          </button>
-        )}
-      </div>
+      <MovieInfinitList></MovieInfinitList>
     </>
   )
 }
---
+++ Project/src/components/movies/MovieInfiinitList.tsx
@@ -0,0 +1,54 @@
+import { useRef, useEffect } from 'react'
+import { useFetchMovies } from '@/hooks/movie.infinite'
+import Loader from '@/components/Loader'
+import { useInView } from 'react-intersection-observer'
+
+export default function MovieInfinitList() {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    searchText,
+    setSearchText,
+    refetch
+  } = useFetchMovies()
+
+  const { ref, inView } = useInView()
+
+  useEffect(() => {
+    if (isLoading) return
+    if (inView) {
+      fetchNextPage()
+    }
+  }, [inView])
+
+  function searchMovies() {
+    refetch()
+  }
+  return (
+    <>
+      <div>
+        <input
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+          onKeyDown={e => e.key === 'Enter' && searchMovies()}
+        />
+        <button onClick={() => searchMovies()}>검색</button>
+        {data?.pages.map(page => {
+          return page.Search.map(movie => {
+            return <div key={movie.imdbID}>{movie.Title}</div>
+          })
+        })}
+        {isLoading && <Loader />}
+        {!isLoading && hasNextPage && (
+          <button
+            ref={ref}
+            onClick={() => fetchNextPage()}>
+            더보기
+          </button>
+        )}
+      </div>
+    </>
+  )
+}
